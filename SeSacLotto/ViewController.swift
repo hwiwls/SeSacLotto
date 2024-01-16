a6613d579fafeb673300ad8d1624d3b33bcb7681
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-01-16 18:44:20 +0900

    Initial Commit

---
+++ SeSacLotto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSacLotto
+//
+//  Created by hwijinjeong on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
